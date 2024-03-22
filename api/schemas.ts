e490c5493345fac841863753b9c79e999013c483
kzhao214 <kzhao214@gmail.com>
2024-03-21 21:12:33 -0400

    Schemas

--- api/schemas.ts
+++ api/schemas.ts
@@ -1,12 +1,58 @@
+import { Image } from 'react-native';
 import Realm, { ObjectSchema } from 'realm';
 
+export class House extends Realm.Object<House> {
+    name!: string;
+    house_code!: string;
+    members!: Realm.List<string>;
+    grocery_list!: GroceryList;
+    receipts!: Realm.List<Receipt>;
+
+    static schema: ObjectSchema = {
+        name: 'House',
+        properties: {
+            name: 'string',
+            house_code: 'string',
+            members: 'Housemate[]',
+            grocery_list: 'GroceryList',
+            receipts: 'Receipt[]',
+        },
+        primaryKey: '_id',
+    };
+}
+
+export class Housemate extends Realm.Object<Housemate> {
+    _id!: string;
+    name?: string;
+    username!: string;
+    password!: string;
+    email?:string;
+
+
+    static schema: ObjectSchema = {
+        name: 'Housemate',
+        properties: {
+            _id: 'string',
+            name: 'string',
+            username: 'string',
+            password: 'string',
+            email: 'string',
+            // inverse relation to House
+            house: {
+                type: 'linkingObjects',
+                objectType: 'House',
+                property: 'members'
+            },
+        },
+        primaryKey: '_id',
+    };
+}
 
 export class GroceryItem extends Realm.Object<GroceryItem> {
     _id!: number;
     name!: string;
     quantity!: number;
-    housemates!: Realm.List<Housemate>;
-    grocerylist!: Realm.List<GroceryList>;
+    price?: number;
 
     static schema: ObjectSchema = {
         name: 'GroceryItem',
@@ -14,46 +60,20 @@ export class GroceryItem extends Realm.Object<GroceryItem> {
             _id: 'int',
             name: 'string',
             quantity: 'int',
-            housemates: 'Housemate[]',
+            price: 'float?',
+            // inverse relation to GroceryList
             grocerylist: {
                 type: 'linkingObjects',
                 objectType: 'GroceryList',
                 property: 'items'
             },
-        },
-    };
-}
-
-export class Housemate extends Realm.Object<Housemate> {
-    _id!: number;
-    name?: string;
-    username!: string;
-    password!: string;
-    email?:string;
-    grocerylist!: Realm.List<GroceryList>;
-    itemlist!: Realm.List<GroceryItem>;
-
-
-    static schema: ObjectSchema = {
-        name: 'Housemate',
-        properties: {
-            _id: 'int',
-            name: 'string',
-            username: 'string',
-            password: 'string',
-            email: 'string',
-            grocerylist: {
+            // inverse relation to Receipt
+            receipt: {
                 type: 'linkingObjects',
-                objectType: 'GroceryList',
-                property: 'housemates'
-            },
-            itemlist: {
-                type: 'linkingObjects',
-                objectType: 'GroceryItem',
-                property: 'housemates'
+                objectType: 'Receipt',
+                property: 'items'
             },
         },
-        primaryKey: '_id',
     };
 }
 
@@ -68,6 +88,30 @@ export class GroceryList extends Realm.Object<GroceryList> {
             _id: 'int',
             housemates: 'Housemate[]',
             items: 'GroceryItem[]'
+        },
+        primaryKey: '_id',
+    };
+}
+
+export class Receipt extends Realm.Object<Receipt> {
+    _id!: number;
+    name!: string;
+    image!: Image;
+    items!: Realm.List<GroceryItem>;
+
+    static schema: ObjectSchema = {
+        name: 'Receipt',
+        properties: {
+            _id: 'int',
+            name: 'string',
+            image: 'Image',
+            items: 'GroceryItem[]',
+            // inverse relation to House
+            house: {
+                type: 'linkingObjects',
+                objectType: 'House',
+                property: 'receipts'
+            },
         },
         primaryKey: '_id',
     };
