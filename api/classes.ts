9f5b54550d401541ba1ece74e3e0bfd1611811c8
kzhao214 <kzhao214@gmail.com>
2024-03-23 16:00:40 -0400

    Temp Schemas with receipts

--- api/classes.ts
+++ api/classes.ts
@@ -1,16 +1,32 @@
+import { Image } from "react-native";
+
 class Housemate {
   userid: string;
   name: string;
   username: string;
   password: string;
-  email: string;
-  grocerylist: string;
+  house_ids: Array<House["housecode"]>;
  
   constructor(name, username, password, email) {
     this.name = name;
     this.username = username;
     this.password = password;
-    this.email = email;
+    this.userid = email;
+  }
+}
+
+class House {
+  name: string;
+  housecode: string;
+  members: Array<Housemate["userid"]>;
+  grocery_list: GroceryList;
+  receipts: Array<Receipt>;
+
+  constructor(name, housecode) {
+    this.name = name;
+    this.housecode = housecode;
+    this.grocery_list = new GroceryList();
+    this.members = [];
   }
 }
 
@@ -18,7 +34,8 @@ class GroceryItem {
   itemid: string;
   name: string;
   quantity: number;
-  HousemateList: Array<Housemate>;
+  price: number;
+  splits: Array<Housemate["userid"]>;
  
   constructor(name, quantity = 1) {
     this.name = name;
@@ -28,14 +45,21 @@ class GroceryItem {
 
 class GroceryList {
   listid: string;
-  joincode: string;
-  name: string;
-  HousemateList: Array<Housemate>;
-  GroceryItemList: Array<GroceryItem>;
+  items: Array<GroceryItem>;
  
+  constructor() {
+    this.items = [];
+  }
+}
+
+class Receipt {
+  receiptid: string;
+  name: string;
+  image: Image;
+  items: Array<GroceryItem>;
+
   constructor(name) {
     this.name = name;
-    this.HousemateList = []
-    this.GroceryItemList = []
+    this.items = [];
   }
 }
