48131941c228fa1e0d29a8535543bef2ef335100
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-03-30 15:17:27 -0400

    backwards direction for ocr

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -25,22 +25,19 @@ def get_receipt_lines(ocr_output, epsilon=5):
         intercept = y1 - slope * x1
 
         return slope, intercept
-    
-    curr_top_slope, curr_top_intercept = calculate_top_slope_and_intercept(ocr_output[0][0])
+
     # all content in a line
     curr_line_content = []
 
     # all lines in a receipt
     receipt_lines = []
 
+    # top --> bottom 
+    """
+    curr_top_slope, curr_top_intercept = calculate_top_slope_and_intercept(ocr_output[0][0])
+
     for line in ocr_output:
-        """
-        line looks like:
-        [
-            [[x1, y1], [x2, y2], [x3, y3], [x4, y4]], (text, confidence)
-        ]
-        [top-left, top-right, bottom-right, bottom-left]
-        """
+       
         bounding_box = line[0]
         pred = line[1]
         text, confidence = pred
@@ -65,7 +62,55 @@ def get_receipt_lines(ocr_output, epsilon=5):
             curr_top_slope = bb_slope
             curr_line_content.append(text)
         else:
+            # curr_top_intercept = bb_intercept
+            # curr_top_slope = bb_slope
             curr_line_content.append(text)
+    """
+
+    # bottom --> top
+    
+    """
+    line looks like:
+        [
+            [[x1, y1], [x2, y2], [x3, y3], [x4, y4]], (text, confidence)
+        ]
+    [top-left, top-right, bottom-right, bottom-left]
+    """
+
+    curr_top_slope, curr_top_intercept = calculate_top_slope_and_intercept(ocr_output[::-1][0][0])
+    # print(ocr_output[::-1][0][0])
+
+    for line in ocr_output[::-1]:
+       
+        bounding_box = line[0]
+        pred = line[1]
+        text, confidence = pred
+
+        bb_slope, bb_intercept = calculate_top_slope_and_intercept(bounding_box)
+
+        bb_middle_x = (bounding_box[0][0] + bounding_box[1][0]) / 2
+        bb_middle_y = (bounding_box[0][1] + bounding_box[1][1]) / 2
+
+        pred_y_val = curr_top_slope*bb_middle_x + curr_top_intercept
+
+
+        difference = abs(bb_middle_y - pred_y_val)
+        
+        if difference > epsilon:
+            # we are on a new line
+            receipt_lines.append(curr_line_content)
+
+            # reset line info
+            curr_line_content = []
+            curr_top_intercept = bb_intercept
+            curr_top_slope = bb_slope
+            curr_line_content.append(text)
+        else:
+            # curr_top_intercept = bb_intercept
+            # curr_top_slope = bb_slope
+            curr_line_content.append(text)
+    
+    receipt_lines.append(curr_line_content) 
 
     return receipt_lines
 
@@ -85,7 +130,7 @@ def scan_receipt(receipt_path, receipt_name):
     boxes = [line[0] for line in result]
     txts = [line[1][0] for line in result]
     scores = [line[1][1] for line in result]
-    im_show = draw_ocr(image, boxes, txts, scores, font_path='./Arial.ttf')
+    im_show = draw_ocr(image, boxes, txts, scores, font_path='ocr\Arial.ttf')
     im_show = Image.fromarray(im_show)
     im_show.save(f'ocr-{receipt_name}.jpg')
 
@@ -94,5 +139,5 @@ def scan_receipt(receipt_path, receipt_name):
     pprint(receipt_lines)
 
 if __name__ == "__main__":
-    scan_receipt("./costco.jpg", "costco")
-    scan_receipt("./hk.jpeg", "hk")
+    scan_receipt("ocr\costco.jpg", "costco")
+    scan_receipt("ocr\hk.jpeg", "hk")
