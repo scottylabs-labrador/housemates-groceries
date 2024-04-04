efd14f16dc45211b9b5445b6e68b5a3c63f75452
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-04-04 00:01:35 -0400

    expo camera setup

--- app/scan.tsx
+++ app/scan.tsx
@@ -1,7 +1,28 @@
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
 export default function Page() {
-    
-    // TODO: Implement the scan page
-    // Camera access and integration with OCR
-    
-    return;
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    // if (!permission) ... 
+
+    // if (!permission.granted) ... 
+
+    function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    }
+
+    return (
+    <View className="flex-1 justify-center">
+        <Camera className="flex-1" type={type}>
+        <View className="flex-1 flex-row bg-transparent m-64">
+            <TouchableOpacity className="flex-1 self-end items-center" onPress={toggleCameraType}>
+            <Text className="text-2xl font-bold text-white">Flip Camera</Text>
+            </TouchableOpacity>
+        </View>
+        </Camera>
+    </View>
+    );
 }
