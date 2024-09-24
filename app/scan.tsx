01c52588915b3aef8c2cf4a653fe745a7b39af56
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-09-23 20:42:36 -0400

    send image to Python backend

--- app/scan.tsx
+++ app/scan.tsx
@@ -12,33 +12,28 @@ export default function Page() {
 
     let RECEIPT_API_URL = 'http://127.0.0.1:8000/receiptLines';
 
-    useEffect(() => {
-        fetch(RECEIPT_API_URL)
-        .then((response) => {
-          response.json().then((data) => {setReceiptLines(data.time);});
-        });
-      }, []);
-
     // if (!permission) ... 
 
     // if (!permission.granted) ... 
 
     function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+      setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
     }
 
     function takePicture() {
       if (camera) {
         camera.takePictureAsync({onPictureSaved: (data) => {
           console.log(data);
-          // fetch(RECEIPT_API_URL, {
-          //   method: 'POST',
-          //   headers: {
-          //     'Content-Type': 'application/json',
-          //   },
-          //   body: JSON.stringify({
-          //   }),
-          // });
+          fetch(RECEIPT_API_URL, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              "image": data.base64
+            }),
+          });
         }});
       }
 
@@ -47,12 +42,12 @@ export default function Page() {
     return (
     <View className="flex-1 justify-center">
         <Camera className="flex-1" type={type} ref={(ref) => {setCamera(ref);}}>
-        <View className="flex-1 flex-row bg-transparent m-64">
-            <Button title="Take Picture" onPress={takePicture}/>
-            <TouchableOpacity className="flex-1 self-end items-center" onPress={toggleCameraType}>
-            <Text className="text-2xl font-bold text-white">Flip Camera</Text>
-            </TouchableOpacity>
-        </View>
+          <View className="flex-1 flex-row bg-transparent m-64">
+              <Button title="Take Picture" onPress={takePicture}/>
+              <TouchableOpacity className="flex-1 self-end items-center" onPress={toggleCameraType}>
+              <Text className="text-2xl font-bold text-white">Flip Camera</Text>
+              </TouchableOpacity>
+          </View>
         </Camera>
     </View>
     );
